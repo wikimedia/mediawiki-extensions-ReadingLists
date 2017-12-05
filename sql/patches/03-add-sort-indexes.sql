34c7ca3e8b5d04743f2e19ae9ba722048e459c3a
Gergő Tisza <gtisza@wikimedia.org>
2017-12-05 22:40:45 +0000

    Fix schema change patch
    Fixes Ia04a6c42ed7eca2a1c49d6ef109f0359db06ccd4.
    
    Change-Id: Icff06fe7dfb7f4f4dc619b281fc07b29389e425b

--- sql/patches/03-add-sort-indexes.sql
+++ sql/patches/03-add-sort-indexes.sql
@@ -6,6 +6,8 @@ DROP INDEX /*i*/rle_user_project_title ON /*_*/reading_list_entry;;
 CREATE INDEX /*i*/rl_user_default ON /*_*/reading_list (rl_user_id, rl_is_default);
 CREATE UNIQUE INDEX /*i*/rl_user_deleted_name_id ON /*_*/reading_list (rl_user_id, rl_deleted, rl_name, rl_id);
 CREATE UNIQUE INDEX /*i*/rl_user_deleted_updated_id ON /*_*/reading_list (rl_user_id, rl_deleted, rl_date_updated, rl_id);
+CREATE UNIQUE INDEX /*i*/rl_user_name_id ON /*_*/reading_list (rl_user_id, rl_name, rl_id);
+CREATE UNIQUE INDEX /*i*/rl_user_updated_id ON /*_*/reading_list (rl_user_id, rl_deleted, rl_date_updated, rl_id);
 CREATE INDEX /*i*/rl_deleted_updated ON /*_*/reading_list (rl_deleted, rl_date_updated);
 
 CREATE UNIQUE INDEX /*i*/rle_list_deleted_title_id ON /*_*/reading_list_entry (rle_rl_id, rle_deleted, rle_title, rle_id);
