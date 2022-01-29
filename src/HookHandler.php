69a4618118d08ded0f39032d35a4c2c8e89fb4a2
Umherirrender <umherirrender_de.wp@web.de>
2022-01-29 21:39:32 +0100

    Convert to abstract schema
    This adds the sqlite and postgres schema to the extension
    
    Bug: T268571
    Change-Id: I40b9114657e44935b51b3fab4f8e8f944bbd1c54

--- src/HookHandler.php
+++ src/HookHandler.php
@@ -41,20 +41,23 @@ class HookHandler {
 	 */
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
 		if ( Utils::isCentralWiki( MediaWikiServices::getInstance() ) ) {
-			$baseDir = dirname( __DIR__ );
-			$patchDir = "$baseDir/sql/patches";
-			$updater->addExtensionTable( 'reading_list', "$baseDir/sql/readinglists.sql" );
-			$updater->dropExtensionTable( 'reading_list_sortkey', "$patchDir/01-drop-sortkeys.sql" );
-			$updater->addExtensionTable( 'reading_list_project',
-				"$patchDir/02-add-reading_list_project.sql" );
-			$updater->addExtensionIndex( 'reading_list', 'rl_user_deleted_name_id',
-				"$patchDir/03-add-sort-indexes.sql" );
-			$updater->dropExtensionField( 'reading_list', 'rl_color',
-				"$patchDir/04-drop-metadata-columns.sql" );
-			$updater->modifyExtensionField( 'reading_list_entry', 'rle_title',
-				"$patchDir/05-increase-rle_title-length.sql" );
-			$updater->addExtensionField( 'reading_list', 'rl_size',
-				"$patchDir/06-add-rl_size.sql" );
+			$type = $updater->getDB()->getType();
+			$baseDir = dirname( __DIR__ ) . '/sql/' . $type;
+			$patchDir = "$baseDir/patches";
+			$updater->addExtensionTable( 'reading_list', "$baseDir/tables-generated.sql" );
+			if ( $type === 'mysql' ) {
+				$updater->dropExtensionTable( 'reading_list_sortkey', "$patchDir/01-drop-sortkeys.sql" );
+				$updater->addExtensionTable( 'reading_list_project',
+					"$patchDir/02-add-reading_list_project.sql" );
+				$updater->addExtensionIndex( 'reading_list', 'rl_user_deleted_name_id',
+					"$patchDir/03-add-sort-indexes.sql" );
+				$updater->dropExtensionField( 'reading_list', 'rl_color',
+					"$patchDir/04-drop-metadata-columns.sql" );
+				$updater->modifyExtensionField( 'reading_list_entry', 'rle_title',
+					"$patchDir/05-increase-rle_title-length.sql" );
+				$updater->addExtensionField( 'reading_list', 'rl_size',
+					"$patchDir/06-add-rl_size.sql" );
+			}
 		}
 		return true;
 	}
@@ -77,8 +80,9 @@ class HookHandler {
 		$originalPrefix = $db->tablePrefix();
 		$db->tablePrefix( $prefix );
 		if ( !$db->tableExists( 'reading_list', __METHOD__ ) ) {
+			$type = $db->getType();
 			$baseDir = dirname( __DIR__ );
-			$db->sourceFile( "$baseDir/sql/readinglists.sql" );
+			$db->sourceFile( "$baseDir/sql/$type/tables-generated.sql" );
 		}
 		$db->tablePrefix( $originalPrefix );
 	}
