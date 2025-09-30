73f7ddb36a10da9e5172a81a954336db918ac54e
Volker E. <volker.e@wikimedia.org>
2025-09-29 21:54:16 -0700

    styles, i18n: Bring empty ReadingList state on par
    Following design, adding SVG image and updating copy text.
    
    Also
    - applying correct BEM syntax to CSS classes
    - remove obsolete message key of now removed CTA button
    
    Bug: T405995
    Change-Id: I875f5e27a1588c3b77d667e2f40c48ecda0f0833

--- resources/ext.readingLists.special/components/EmptyList.vue
+++ resources/ext.readingLists.special/components/EmptyList.vue
@@ -1,36 +1,22 @@
 <template>
 	<div class="reading-lists-empty">
-		<h2 class="reading-lists-empty-title">
+		<h2 class="reading-lists-empty__title">
 			{{ msgEmptyTitle }}
 		</h2>
 
-		<p class="reading-lists-empty-cta">
-			{{ msgEmptyCta }}
+		<p class="reading-lists-empty__description">
+			{{ msgEmptyDescription }}
 		</p>
-
-		<a :href="randomUrl">
-			<cdx-button action="progressive" weight="primary">
-				<cdx-icon :icon="cdxIconDie"></cdx-icon>
-				{{ msgEmptyRandom }}
-			</cdx-button>
-		</a>
 	</div>
 </template>
 
 <script>
-const { CdxButton, CdxIcon } = require( '../../../codex.js' );
-const { cdxIconDie } = require( '../../../icons.json' );
-
 // @vue/component
 module.exports = exports = {
-	components: { CdxButton, CdxIcon },
 	setup() {
 		return {
-			randomUrl: mw.util.getUrl( 'Special:Random' ),
 			msgEmptyTitle: mw.msg( 'readinglists-empty-title' ),
-			msgEmptyCta: mw.msg( 'readinglists-empty-cta' ),
-			msgEmptyRandom: mw.msg( 'readinglists-empty-random' ),
-			cdxIconDie
+			msgEmptyDescription: mw.msg( 'readinglists-empty-cta' )
 		};
 	}
 };
