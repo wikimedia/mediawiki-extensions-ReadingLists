f82cdf10c15c19d87daf89679481515c57c20a83
aude <aude.wiki@gmail.com>
2026-06-24 09:04:05 -0400

    Remove Lists.vue from index.js in ext.readingLists.special
    This code path was unreachable since the SpecialReadingLists.php
    redirects Special:ReadingLists to Special:ReadingLists/{user_name}.
    
    Remove Lists.vue and ListItem.vue now that they are unused,
    and associated tests.
    
    Bug: T405393
    Change-Id: I51a415fa23c308ef4d2b1f8b15537e23b3da59b8

--- resources/ext.readingLists.special/index.js
+++ resources/ext.readingLists.special/index.js
@@ -1,6 +1,5 @@
 const api = require( 'ext.readingLists.api' );
 const { createMwApp } = require( 'vue' );
-const Lists = require( './pages/Lists.vue' );
 const Entries = require( './pages/Entries.vue' );
 
 let page = mw.config.get( 'wgPageName' );
@@ -11,34 +10,31 @@ if ( page.endsWith( '/' ) ) {
 
 const parts = page.split( '/' );
 
-async function mountApp() {
-	let app;
-
-	// show imported list, if limport or lexport parameter is present,
-	// otherwise the php special page redirects to Special:ReadingLists/{user_name}.
-	// keeping the logic for Lists.vue here but now we redirect
-	// Special:ReadingLists to Special:ReadingLists/{user_name} per T400361.
+async function createApp() {
+	// Show imported list if limport or lexport parameter is present.
+	// Otherwise the PHP special page redirects to Special:ReadingLists/{user_name}.
 	if ( parts.length < 2 ) {
 		const search = new URLSearchParams( window.location.search );
 		const imported = search.get( 'limport' ) || search.get( 'lexport' );
 
-		if ( imported === null ) {
-			app = createMwApp( Lists );
-		} else {
-			app = createMwApp( Entries, {
+		if ( imported ) {
+			return createMwApp( Entries, {
 				imported: await api.fromBase64( imported )
 			} );
 		}
-	// show specific reading list (default or custom, based on reading list id)
-	// e.g. Special:ReadingLists/{user_name}/{list_id}
 	} else if ( parts.length >= 3 ) {
-		app = createMwApp( Entries, {
+		// show specific reading list (default or custom, based on reading list id)
+		// e.g. Special:ReadingLists/{user_name}/{list_id}
+		return createMwApp( Entries, {
 			listId: parseInt( parts[ 2 ] )
 		} );
-	// show all items from all reading lists on Special:ReadingLists/{user_name} page
-	} else {
-		app = createMwApp( Entries );
 	}
+
+	return createMwApp( Entries );
+}
+
+async function mountApp() {
+	const app = await createApp();
 
 	app.mount( '.reading-lists-container' );
 }
